63b9a7a492513ba8ed8b70e0efda9471f7afa710
ragesh DevOps <Ragesh@devops.com>
2023-11-02 22:05:00 +0530

    local tags

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,21 @@
+locals {
+  common_tags = {
+    owner = "devops"
+    service = "backend"
+  }
+}
+#----------------Don't Touch the Tags----------------------------------
+
 resource "aws_instance" "applications" {
   ami           = var.ami_ids["windows"] # Use the [Ubuntu]
   instance_type = "t2.micro"
-  tags = {
-    name = "my application"
-  }
+  tags = local.common_tags
+    
 }
 
 resource "aws_instance" "ubuntumachine" {
   ami           = var.ami_ids["ubuntu"] # Use the [Ubuntu]
   instance_type = "t2.micro"
-  tags = {
-    name = "my application ubuntu"
-  }
+  tags = local.common_tags
 }
 
